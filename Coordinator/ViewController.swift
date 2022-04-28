f264eb5c7a8fdbf4c773c481d3da806a372b1cd4
Sosina Tatyana <fedorova_t95@mail.ru>
2022-04-28 11:34:12 +0300

    Initial Commit

---
+++ Coordinator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coordinator
+//
+//  Created by Tatiana Sosina on 28.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
